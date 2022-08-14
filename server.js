b73781efa54180266c4a80e628f07f9c4ba3d6b9
JacquelineCristina <jacqueline.jcsilva@gmail.com>
2022-08-14 14:19:40 -0300

    teste deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/poupafacil-frontend-poc'));
+app.use(express.static('./dist/poupafacil-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/poupafacil-frontend-poc/'}),
+    res.sendFile('index.html', {root: 'dist/poupafacil-front/'}),
 );
 
 // Start the app by listening on the default Heroku port
